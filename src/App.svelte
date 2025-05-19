ea57c1c984bb7d71656f4fa6e751fedccde1f989
Laddge <dev.laddge@gmail.com>
2025-05-20 00:23:20 +0900

    feat: add auto mode

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,9 @@
   import qrcode, { type QRCodeSegment, type QRCodeErrorCorrectionLevel } from 'qrcode'
 
   let data: QRCodeSegment[] = [{ data: '', mode: 'alphanumeric' }]
+  let dataStr = ''
   let errorCorrectionLevel: QRCodeErrorCorrectionLevel ='L'
+  let isManualMode = false
   let dataURL = ''
 
   function append() {
@@ -16,13 +18,23 @@
   }
 
   $: {
-    qrcode.toDataURL(data, { errorCorrectionLevel }, (err, url) => {
-      if (err) {
-        dataURL = ''
-      } else {
-        dataURL = url
-      }
-    })
+    if (isManualMode) {
+      qrcode.toDataURL(data, { errorCorrectionLevel }, (err, url) => {
+        if (err) {
+          dataURL = ''
+        } else {
+          dataURL = url
+        }
+      })
+    } else {
+      qrcode.toDataURL(dataStr, { errorCorrectionLevel }, (err, url) => {
+        if (err) {
+          dataURL = ''
+        } else {
+          dataURL = url
+        }
+      })
+    }
   }
 </script>
 
@@ -32,27 +44,36 @@
     <ul class="list-disc list-inside mb-4"><li><a href="https://github.com/soldair/node-qrcode" target="_blank" class="underline">node-qrcode</a></li></ul>
     <h2 class="text-xl font-bold mb-4">Data</h2>
     <div class="mb-4">
-      <table class="table">
-        <tbody>
-          {#each data as d, i}
-            <tr>
-              <th>{i+1}</th>
-              <td>
-                <input bind:value={d.data} class="input" />
-              </td>
-              <td>
-                <select bind:value={d.mode} class="select">
-                  <option>alphanumeric</option>
-                  <option>numeric</option>
-                  <option>byte</option>
-                </select>
-              </td>
-              <td><button onclick={() => remove(i)} class="btn btn-soft btn-error">x</button></td>
-            </tr>
-          {/each}
-        </tbody>
-      </table>
-      <button onclick={append} class="btn btn-soft btn-primary">append</button>
+      <div class="mb-4">
+        <label><input type="checkbox" bind:checked={isManualMode} class="checkbox" /> Manual Mode</label>
+      </div>
+      {#if isManualMode}
+        <table class="table">
+          <tbody>
+            {#each data as d, i}
+              <tr>
+                <th>{i+1}</th>
+                <td>
+                  <input bind:value={d.data} class="input" />
+                </td>
+                <td>
+                  <select bind:value={d.mode} class="select">
+                    <option>alphanumeric</option>
+                    <option>numeric</option>
+                    <option>byte</option>
+                  </select>
+                </td>
+                <td><button onclick={() => remove(i)} class="btn btn-soft btn-error">x</button></td>
+              </tr>
+            {/each}
+          </tbody>
+        </table>
+        <button onclick={append} class="btn btn-soft btn-primary">append</button>
+      {:else}
+        <div>
+          input: <input bind:value={dataStr} class="input" />
+        </div>
+      {/if}
     </div>
     <h2 class="text-xl font-bold mb-4">Option</h2>
     <div class="mb-4">
@@ -65,7 +86,7 @@
       </select>
     </div>
     <div class="mb-4">
-      <div class="text-sm text-base-content/50">{JSON.stringify(data)}, {'{'} "errorCorrectionLevel": "{errorCorrectionLevel}" {'}'}</div>
+      <div class="text-sm text-base-content/50">{isManualMode ? JSON.stringify(data) : dataStr}, {'{'} "errorCorrectionLevel": "{errorCorrectionLevel}" {'}'}</div>
     </div>
     <h2 class="text-xl font-bold mb-4">Result</h2>
     <div class="mb-4">
